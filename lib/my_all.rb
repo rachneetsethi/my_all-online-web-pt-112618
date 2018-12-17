b56961c570cc9bddb64bdc912d3525aa5853b1f4
Rachneet <rachneetsethi@gmail.com>
2018-12-17 05:37:59 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -19,3 +19,13 @@ def my_all?([1,2,3])
 end
 
 my_all?([1,2,3]) {|1| 1 < 2}
+
+def my_all?(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
+end
+
